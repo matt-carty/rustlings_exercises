b7808e79ace3b32f2d94d4df8acadd34cb995631
Matt <matt@cartycodes.com>
2024-06-04 15:27:15 +0900

    finished quiz 1

--- quiz1.rs
+++ quiz1.rs
@@ -13,11 +13,12 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
-
+fn calculate_price_of_apples(number_of_apples: i32) -> i32 {
+    if number_of_apples > 40 { number_of_apples }
+    else { number_of_apples * 2 }
+}
 // Don't modify this function!
 #[test]
 fn verify_test() {
