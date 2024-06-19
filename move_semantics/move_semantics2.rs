2ae9b90f234dda1595c94d049cac161b7a02eda1
Matt <matt@cartycodes.com>
2024-06-19 12:54:41 +0900

    regular commit

--- move_semantics/move_semantics2.rs
+++ move_semantics/move_semantics2.rs
@@ -5,13 +5,12 @@
 // Execute `rustlings hint move_semantics2` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
 
 #[test]
 fn main() {
     let vec0 = vec![22, 44, 66];
 
-    let mut vec1 = fill_vec(vec0);
+    let vec1 = fill_vec(vec0.clone());
 
     assert_eq!(vec0, vec![22, 44, 66]);
     assert_eq!(vec1, vec![22, 44, 66, 88]);
@@ -22,5 +21,5 @@ fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
 
     vec.push(88);
 
-    vec
+     vec
 }
